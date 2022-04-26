4438b856c1d5ac1aa7a18490e6fac006f9ecd8a5
Vladislav Gavaza <78958805+Azavag@users.noreply.github.com>
2022-04-26 10:18:12 +0300

    Update settings.py

--- Django/Django/settings.py
+++ Django/Django/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '888e63e6-dba6-4010-bc72-fdb4bf620846'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
