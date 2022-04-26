787d966d86d9f0bf83e1bad97232b727116032c1
Vladislav Gavaza <78958805+Azavag@users.noreply.github.com>
2022-04-26 03:27:37 +0300

    Delete Django/env directory

--- Django/env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!C:\Users\Admin\Desktop\Думфист\Анаконда\Django\Django\env\Scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
